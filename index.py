1c4761a45c8a7a56a6abb892bb278a04c9c121de
Debayan Mitra <debayan.mitra888@gmail.com>
2025-11-23 20:18:43 +0530

    Final updates on code simplification and variable simplification

--- index.py
+++ index.py
@@ -1,91 +1,220 @@
-details = []
+import random
+import time
 
-def create_account():
-    global account
-    name = input("Enter your ID registered name: ")
-    deposit = float(input("Enter deposit amount: "))
-    pin = input("Set a 4-digit PIN for your account: ")
-    if len(details) != 0:
-        ac = details[len(details)-1]['account'] + 1
-    else:
-        ac=1000
-    account = {'name': name,'account':ac,'balance': deposit, 'pin':pin}
-    details.append(account)
-    print(f"Account created successfully for {name} having account number {ac} with balance Rs.{deposit:.2f}")
+ac_list = []
 
-def view_balance(ac):
-    if not details:
-        print("No accounts found. Please create an account first.")
-        return
-    pin = input("Enter your 4-digit PIN: ")
-    for acc in details:
-        if acc['account'] == ac and pin==acc['pin']:
-            print(f"Account Balance for {acc['name']}: Rs.{acc['balance']:.2f}")
-            return
-    print("Account not found.")
-def show_accounts():
-    print(details)
 
-def transfer_funds():
-    if not details:
-        print("No accounts found. Please create an account first.")
-        return
-    ac = int(input("Enter your account number: "))
-    pin = input("Enter your 4-digit PIN: ")
-    for acc in details:
-        if acc['account'] == ac and pin==acc['pin']:
-            to_ac = int(input("Enter recipient's account number: "))
-            if to_ac == ac:
-                print("Cannot transfer to the same account.")
-                #to_ac = int(input("Enter recipient's account number: "))
-                continue
-            amount = float(input("Enter amount to transfer: "))
-            if acc['balance'] >= amount:
-                for rec_acc in details:
-                    print(f"Recipient Name: {rec_acc['name']}")
-                    chs = input("Confirm name? (yes/no): ")
-                    chs = chs.lower().strip()
-                    if chs == 'yes':
-                        acc['balance'] -= amount
-                        rec_acc['balance'] += amount
-                        print(f"Transferred Rs.{amount:.2f} from account {ac} to account {to_ac}.")
-                        print(f"New balance for account {ac}: Rs.{acc['balance']:.2f}")
-                    else:
-                        print("Transfer cancelled by user.")
-                    return
-                print("Recipient account not found.")
-                    
-                return
+
+def find_ac(acno):
+    
+    """Finds account by number."""
+    for ac in ac_list:
+        if ac['id'] == acno:
+            return ac 
+    return None 
+
+def safe_input(txt, type, tries = 3):
+    """Gets input safely with retries."""
+    for i in range(tries):
+        try:
+            inp = input(txt).strip()
+            
+            if type == float:
+                val = float(inp)
+                if val <= 0:
+                    print("Error: Amount must be > 0.")
+                    continue
+                return val
+            
+            return type(inp)
+        
+        except ValueError:
+            print(f"Error: Wrong input format.")
+            if i < tries - 1:
+                print(f"Try again. {tries - 1 - i} tries left.")
             else:
-                print("Insufficient balance.")
-                return
-    print("Account not found. or incorrect PIN.")
+                print("Failed. Restarting transaction.")
+    return None  
 
+def login():
+    """Checks ID and PIN."""
+    print("\n--- Login ---")
+    
+    # Get ID
+    acno = safe_input("Enter Account No (4 digit): ", str)
+    if acno is None: 
+        return None
 
+    # Find account
+    ac = find_ac(acno)
+    if ac is None:
+        print("Error: Account not found.")
+        return None
 
-while True:
-    print("==============================")
-    print("Welcome to the Banking Page!")
-    print("==============================")
-    print("Here you can manage your bank accounts and view transactions.")
-    print("Please choose an option from the menu below:")
-    print("1. Create New Account")    
-    print("2. View Account Balance")
-    print("3. Transfer Funds")
-    print("4. Exit")
-    ch=input("Enter your choice (1-4): ")
-    if ch=='1':
-        create_account()
-    elif ch=='2':
-        ac=int(input("Enter your account number: "))
-        view_balance(ac)
-    elif ch=='3':
-        transfer_funds()
-    elif ch=='4':
-        print("Exiting Banking Page. Have a great day!")
-        break
-    elif ch=='show':
-        show_accounts()
+    # Get PIN
+    p = safe_input("Enter PIN (4 digit): ", str)
+    if p is None: 
+        return None
+    
+    # Match PIN
+    if ac['pin'] == p:
+        print(f"Success! Welcome, {ac['name']}.")
+        return ac
     else:
-        print("Invalid choice. Please try again; if you want to exit press 4")
+        print("Error: Wrong PIN.")
+        return None
 
+def new_ac():
+    """Makes a new account."""
+    print("\n--- New Account ---")
+    
+    nm = input("Enter Name: ")
+    if not nm: 
+        return print("Failed.")
+    
+    print("Min balance is ₹ 500.00")
+    bal=0
+    bal = safe_input("Enter Deposit: ₹", float)
+    if not bal or bal < 500:
+        return print("Failed.")
+    
+    p = input("Set 4-digit PIN: ").strip()
+    if len(p) != 4 or not p.isdigit():
+        print("Error: PIN must be 4 digits.")
+        return print("Failed.")
+    
+    """Generates a unique 4-digit account number."""
+    while True:
+        num = str(random.randint(1000, 9999))
+        
+        # Check if ID is unique
+        if not find_ac(num):
+            id = num
+            break
+        
+    data = {"id": id,"pin": p,"name": nm,"bal": bal}
+    
+    ac_list.append(data)
+    
+    print("\nCreated!")
+    print(f"Ac No: {id}")
+    print(f"Name: {nm}")
+    print(f"Bal: ₹ {bal:,.2f}")
+
+def show_bal():
+    """Shows balance."""
+    ac = login()
+    if ac is not None:
+        print("\n--- Balance Info ---")
+        print(f"Name: {ac['name']}")
+        print(f"Ac No: {ac['id']}")
+        print(f"Balance: ₹ {ac['bal']:,.2f}")
+        
+def add_money():
+    """Deposit money."""
+    ac = login()
+    if ac is None: return print("Failed.")
+    
+    print(f"\n--- Deposit to {ac['id']} ---")
+    
+    amt = safe_input("Amount to add: ₹", float)
+    if amt is None: return print("Failed.")
+    
+    ac['bal'] += amt
+    print(f"\nDone. Added ₹ {amt:,.2f}.")
+    print(f"New Bal: ₹ {ac['bal']:,.2f}")
+
+def wdraw():
+    """Withdraw money."""
+    ac = login()
+    if ac is None: return print("Failed.")
+    
+    print(f"\n--- Withdraw from {ac['id']} ---")
+    
+    amt = safe_input("Amount to take: ₹", float)
+    if amt is None: return print("Failed.")
+    
+    if amt > ac['bal']:
+        print("Error: Not enough money.")
+        print(f"Bal is: ₹ {ac['bal']:,.2f}")
+        return print("Failed.")
+        
+    ac['bal'] -= amt
+    print(f"\nDone. Took ₹ {amt:,.2f}.")
+    print(f"New Bal: ₹ {ac['bal']:,.2f}")
+
+def trans():
+    """Transfer money."""
+    me = login()
+    if me is None: return print("Failed.")
+    
+    print(f"\n--- Transfer from {me['id']} ---")
+    
+    other_no = input("To Account No: ").strip()
+    other = find_ac(other_no)
+    
+    if other is None:
+        return print("Error: Receiver not found.")
+    
+    if me['id'] == other['id']:
+        return print("Error: Can't send to self.")
+
+    amt = safe_input("Amount to send: ₹", float)
+    if amt is None: return print("Failed.")
+
+    if amt > me['bal']:
+        print("Error: Not enough money.")
+        return print("Failed.")
+
+    #Money transfer process (+ on receiver, - on sender)
+    me['bal'] -= amt
+    other['bal'] += amt
+    
+    #Print the account transfer details and money transfer confirmation
+    print("\nSent Successfully.")
+    print(f"Amount: ₹ {amt:,.2f}")
+    print(f"To: {other['name']}")
+    print(f"My New Bal: ₹ {me['bal']:,.2f}")
+
+
+def main():
+    while True:
+        print()
+        print("="*40)
+        print("             BANKING SYSTEM         ")
+        print("="*40)
+        print("Welcome to the Customer Banking Services!")
+        print("-"*40)
+        print("Please choose an option below:")
+        print("-"*40)
+        print("1. New Account")
+        print("2. Check Balance")
+        print("3. Deposit")
+        print("4. Withdraw")
+        print("5. Transfer")
+        print("6. Exit")
+        print("-" * 30)
+
+        opt = input("Choose (1-6): ").strip()
+        
+        if opt == '1':
+            new_ac()
+        elif opt == '2':
+            show_bal()
+        elif opt == '3':
+            add_money()
+        elif opt == '4':
+            wdraw()
+        elif opt == '5':
+            trans()
+        elif opt == '6':
+            print("\nBye! Good to see you and have a nice day.")
+            break 
+        else:
+            print("Invalid Choice Please try again!")
+            
+        if opt in ['1', '2', '3', '4', '5']:
+            print("\n")
+            time.sleep(1) 
+
+main()
